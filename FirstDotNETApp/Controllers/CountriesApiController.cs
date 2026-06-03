e94cf27600fef3d74e11a73d29e5639ec0a8ea54
devabarenyagogoi <devabarenya@gmail.com>
2026-06-03 07:39:57 +0530

    some tweaks in api

---
+++ FirstDotNETApp/Controllers/CountriesApiController.cs
@@ -0,0 +1,41 @@
+﻿using FirstDotNETApp.Interfaces;
+using FirstDotNETApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstDotNETApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesApiController : ControllerBase
+    {
+        private readonly ICountryService _countryService;
+
+        public CountriesApiController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCountries()
+        {
+            var countries =
+                await _countryService.GetAllCountriesAsync();
+
+            return Ok(countries);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCountry([FromBody] CountryViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _countryService.CreateCountryAsync(vm);
+
+            return Ok(vm);
+        }
+    }
+
+}
